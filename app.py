bb21cc6f35d6ba5bdd921179a97c93aff2f2814e
PrajwalSahu <116376785+PRgb2022@users.noreply.github.com>
2025-08-04 16:10:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -82,5 +82,5 @@ def query():
         return jsonify({'error': f'Error running query: {str(e)}'}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5001))
     app.run(debug=True, host="0.0.0.0", port=port)
